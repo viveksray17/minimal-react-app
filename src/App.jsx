d8b5e9176e7364b67fe37e2573cc79b93f69736d
Vivek Sundaray <viveksundaray123@outlook.com>
2022-11-22 16:08:53 +0530

    fixed some errors

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,26 @@ import { Component } from "react";
 
 class App extends Component {
 	state = {
-		name: "vivek sundaray"
+		name: "Vivek",
 	};
 
 	name2 = () => {
-		this.setState({name: "John Smith"})
-	}
+		this.setState({ name: "John Smith" });
+	};
 	name1 = () => {
-		this.setState({name: "Vivek Sundaray"})
-	}
+		this.setState({ name: "Vivek Sundaray" });
+	};
 
 	render() {
-		const initialName = this.state.name
-		<div>
-			<span>{initialName}</span>
-			<button onClick={this.name2}>name2</button>
-			<button onClick={this.name1}>name1</button>
-		</div>
+		const initialName = this.state.name;
+		return (
+			<div>
+				<span>{initialName}</span>
+				<button onClick={this.name1}>name1</button>
+				<button onClick={this.name2}>name2</button>
+			</div>
+		);
 	}
 }
 
-export default Counter;
+export default App;
