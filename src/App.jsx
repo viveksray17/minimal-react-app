849b06ae85847697031d67b7e45789c293a0d6c3
Vivek Sundaray <viveksundaray123@outlook.com>
2022-11-23 14:02:58 +0530

    made it minimal

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
 import { Component } from "react";
 
 class App extends Component {
-	state = {
-		name: "Vivek",
-	};
-	changeName = () => {
-		if (this.state.name === "Vivek"){
-			this.setState({name: "John"})
-		}else{
-			this.setState({name: "Vivek"})
-		}
-	}
 	render() {
-		return (
-			<div className="container">
-				<div>{this.state.name}</div>
-				<button onClick={this.changeName} className="btn btn-primary">ChangeName</button>
-			</div>
-		);
+		return <div>Hello World</div>;
 	}
 }
 
