a8d5ed0e375cc29cdb502804dc72adc31df05688
Vivek Sundaray <viveksundaray123@outlook.com>
2022-11-24 13:04:57 +0530

    use react fragment

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function App(){
 	const [name, setName] = useState("vivek");
-	return <div>Hello {name}</div>
+	return <React.Fragment>Hello {name}</React.Fragment>
 }
 
 export default App;
